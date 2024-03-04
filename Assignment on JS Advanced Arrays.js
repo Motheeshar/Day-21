873ea6dacddc038742c58f5574387615d492312c
A R Motheesh <ar.motheesh@gmail.com>
2024-03-05 04:38:11 +0530

    Update Assignment on JS Advanced Arrays.js

--- Assignment on JS Advanced Arrays.js
+++ Assignment on JS Advanced Arrays.js
@@ -1,13 +1,13 @@
 var listOfActivities = [];
-
+//This array will store the list of activities entered by the user.
 var input = document.getElementById("input");
-
+//This variable references the input field in the HTML.
 var todolist = document.getElementById("todolist");
-
+//This variable references the unordered list in the HTML.
 document.getElementById("button").onclick = click;
-
+//This line assigns the click function to the click event of the button.
 function click() {
-
+//This function is called when the button is clicked.
   listOfActivities.push(input.value);
   console.log(listOfActivities);
   input.value = "";
